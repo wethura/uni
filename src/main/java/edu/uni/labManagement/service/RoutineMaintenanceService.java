0af3382913c2e056a6928360ea52a63b35460e60
sola <827757772@qq.com>
2019-05-06 00:11:58 +0800

    2019年5月5日17点09分

---
+++ src/main/java/edu/uni/labManagement/service/RoutineMaintenanceService.java
@@ -0,0 +1,10 @@
+package edu.uni.labManagement.service;
+
+/**
+ * Create by Administrator
+ *
+ * @author sola
+ * @date 2019/05/06 0:11
+ */
+public interface RoutineMaintenanceService {
+}
---
+++ src/main/java/edu/uni/labManagement/service/impl/RoutineMaintenanceServiceImpl.java
@@ -0,0 +1,15 @@
+package edu.uni.labManagement.service.impl;
+
+import edu.uni.labManagement.service.RoutineMaintenanceService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Create by Administrator
+ *
+ * @author sola
+ * @date 2019/05/06 0:11
+ */
+@Service
+public class RoutineMaintenanceServiceImpl implements RoutineMaintenanceService {
+
+}
